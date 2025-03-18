8d88f85280ee2136fd1f925d95b571848c13cfbd
Hirad <hiradkazem@gmail.com>
2025-03-18 16:34:47 +0330

    Update httpp.py

--- httpp.py
+++ httpp.py
@@ -1,10 +1,18 @@
-import http.server
+mport http.server
 import socketserver
+import os
 
-PORT = 8000
+def httpp():
+    class CustomHandler(http.server.SimpleHTTPRequestHandler):
+        def do_GET(self):
+            if self.path == "CHECK.txt" :
+                self.path = "CHECK.txt"
+                return http.server.SimpleHTTPRequestHandler.do_GET(self)
+            else :
+                self.send_error(404, "File not found")
 
-Handler = http.server.SimpleHTTPRequestHandler
+    PORT = 8000
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Server started at http://192.168.1.55:{PORT}")
-    httpd.serve_forever()
+    with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+        print(f"Server started at http://192.168.1.55:{PORT}")
+        httpd.serve_forever()
